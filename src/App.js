23e1e951c52db0c2c4fcc20b21343c56b76989a2
Slaupsolution <slaupbr@gmail.com>
2025-03-19 12:51:51 -0300

    Add bottom navigation and login screen styles

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Verde Marketplace</h1>
+      </header>
+      <main>
+        <p>Welcome to Verde Marketplace</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
